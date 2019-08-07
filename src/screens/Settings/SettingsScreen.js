f33894279ab8a613dce83c718ca70a9f50a220be
Arthur Martello <arthurmartello@outlook.com>
2019-08-07 11:46:24 +0200

    Use redux for settings

--- src/screens/Settings/SettingsScreen.js
+++ src/screens/Settings/SettingsScreen.js
@@ -6,15 +6,17 @@
  */
 
 import React from 'react';
-import { Alert, ScrollView, Text, View } from 'react-native';
+import { Alert, RefreshControl, ScrollView, Text, View } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import { connect } from 'react-redux';
 import colors from '../../styles/colors';
 import BlockTemplate from '../../components/BlockTemplate';
 import { _, Settings as t } from '../../utils/i18n';
 import SwitchBlockTemplate from '../../components/SwitchBlockTemplate';
-import PayUTC from '../../services/PayUTC';
+import { Config, PayUTC } from '../../redux/actions';
+import PayUTCApi from '../../services/PayUTC';
 
-export default class SettingsScreen extends React.PureComponent {
+class SettingsScreen extends React.PureComponent {
 	static navigationOptions = {
 		title: t('title'),
 		header: null,
@@ -23,26 +25,46 @@ export default class SettingsScreen extends React.PureComponent {
 
 	constructor(props) {
 		super(props);
-		this.state = { lock: null };
+
+		this.state = { expectedStatus: false };
+
 		this.onLockChange = this.onLockChange.bind(this);
 	}
 
 	componentDidMount() {
-		PayUTC.getLockStatus().then(([status]) => {
-			if (status === true || status === false) {
-				this.setState({ lock: status });
-			}
-		});
+		this.onRefresh();
+	}
+
+	onRefresh() {
+		const { lockStatusFetching, lockStatus, dispatch } = this.props;
+
+		if (!lockStatusFetching) {
+			dispatch(PayUTC.getLockStatus());
+			this.setState({ expectedStatus: !lockStatus });
+		}
 	}
 
 	onLockChange(value) {
-		const { lock } = this.state;
+		const { dispatch, lockStatusFetching } = this.props;
 
-		if (lock == null) {
+		if (lockStatusFetching) {
 			return;
 		}
 
-		PayUTC.setLockStatus(value).then(([status]) => {
+		dispatch(
+			Config.spinner({
+				visible: true,
+				textContent: value ? t('locking') : t('unlocking'),
+			})
+		);
+
+		PayUTCApi.setLockStatus(value).then(([status]) => {
+			dispatch(
+				Config.spinner({
+					visible: false,
+				})
+			);
+
 			if (status !== true && status !== false) {
 				Alert.alert(
 					_('error'),
@@ -52,7 +74,7 @@ export default class SettingsScreen extends React.PureComponent {
 				);
 				return;
 			}
-			this.setState({ lock: status });
+			this.onRefresh();
 		});
 	}
 
@@ -63,11 +85,21 @@ export default class SettingsScreen extends React.PureComponent {
 	}
 
 	render() {
-		const { lock } = this.state;
-		const { navigation } = this.props;
+		const { lockStatus, lockStatusFetching, navigation } = this.props;
+		const { expectedStatus } = this.state;
 
 		return (
-			<ScrollView style={{ backgroundColor: colors.backgroundLight, padding: 15 }}>
+			<ScrollView
+				style={{ backgroundColor: colors.backgroundLight, padding: 15 }}
+				refreshControl={
+					<RefreshControl
+						refreshing={lockStatusFetching}
+						onRefresh={() => this.onRefresh()}
+						colors={[colors.secondary]}
+						tintColor={colors.secondary}
+					/>
+				}
+			>
 				<BlockTemplate roundedTop roundedBottom shadow>
 					<Text style={{ fontSize: 22, fontWeight: 'bold', color: colors.primary }}>
 						{t('title')}
@@ -77,23 +109,26 @@ export default class SettingsScreen extends React.PureComponent {
 				<SwitchBlockTemplate
 					roundedTop
 					roundedBottom
-					value={lock == null ? false : lock}
+					value={lockStatusFetching ? expectedStatus : lockStatus}
 					onValueChange={this.onLockChange}
 					tintColor={colors.less}
-					disabled={lock == null}
+					disabled={lockStatusFetching}
 				>
 					<View style={{ flex: 1, flexDirection: 'column' }}>
 						<Text
 							style={{
 								fontSize: 16,
 								fontWeight: 'bold',
-								color: lock == null ? colors.disabled : colors.secondary,
+								color: lockStatusFetching ? colors.disabled : colors.secondary,
 							}}
 						>
 							{t('lock')}
 						</Text>
 						<Text
-							style={{ fontSize: 13, color: lock == null ? colors.disabled : colors.secondary }}
+							style={{
+								fontSize: 13,
+								color: lockStatusFetching ? colors.disabled : colors.secondary,
+							}}
 						>
 							{t('lock_info')}
 						</Text>
@@ -130,3 +165,15 @@ export default class SettingsScreen extends React.PureComponent {
 		);
 	}
 }
+
+const mapStateToProps = ({ payutc }) => {
+	const lockStatus = payutc.getLockStatus();
+
+	return {
+		lockStatus: lockStatus.getData(),
+		lockStatusFetching: lockStatus.isFetching(),
+		lockStatusFetched: lockStatus.isFetched(),
+	};
+};
+
+export default connect(mapStateToProps)(SettingsScreen);
