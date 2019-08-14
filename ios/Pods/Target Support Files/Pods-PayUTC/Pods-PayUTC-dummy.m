1fa2a1c9594dd700154699ba39bc7c3f9b1a4ff7
Arthur Martello <arthurmartello@outlook.com>
2019-08-15 01:15:34 +0200

    Remove Pods

--- ios/Pods/Target Support Files/Pods-PayUTC/Pods-PayUTC-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PayUTC : NSObject
-@end
-@implementation PodsDummy_Pods_PayUTC
-@end
--- ios/Pods/Target Support Files/Pods-PayUTCTests/Pods-PayUTCTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PayUTCTests : NSObject
-@end
-@implementation PodsDummy_Pods_PayUTCTests
-@end
