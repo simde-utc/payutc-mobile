af8802c5cefbc72c2ee12cf59824463affc02382
Arthur Martello <arthurmartello@outlook.com>
2019-08-15 00:32:51 +0200

    Resolve auto-correction for Messages form

--- src/components/Transfer/MessageForm.js
+++ src/components/Transfer/MessageForm.js
@@ -111,7 +111,6 @@ export default class MessageForm extends React.Component {
 					maxLength={this.maxLength}
 					multiline
 					textContentType="none"
-					autoCorrect={false}
 					onChangeText={text => this.onChange(text)}
 					value={text}
 				/>
