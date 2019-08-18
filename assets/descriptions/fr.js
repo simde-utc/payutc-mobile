9d2ce477695785f99cc4c94f836bf93e321245ba
NastuzziSamy <samy@nastuzzi.fr>
2019-08-18 19:08:40 +0200

    Add descriptions, in fr and en

---
+++ assets/descriptions/fr.js
@@ -0,0 +1,13 @@
+export const title = 'PayUTC';
+export const short = `Le porte-monnaie des associations de l'Université de Technologie de Compiègne`;
+export const description = `L'Université de Technologie de Compiègne et ses associations utilisent le système cashless PayUTC.
+
+Via cette application, chaque membre de l'UTC et des écoles partenaires pourront se connecter pour accéder à leur compte et leur porte-monnaie électronique.
+
+Il est possible notamment de:
+- Consulter son solde
+- Consulter son historique
+- Transférer de l’argent aux autres membres
+- Recharger directement depuis une interface sécurisée
+- Obtenir des statistiques basées sur son compte
+- Verrouiller son badge`;
