bc3714edccbb38f27ee75ac129c4937e4774523b
NastuzziSamy <samy@nastuzzi.fr>
2019-08-18 22:22:01 +0200

    Full lower case search

--- src/screens/History/HistoryScreen.js
+++ src/screens/History/HistoryScreen.js
@@ -87,7 +87,8 @@ class HistoryScreen extends React.Component {
 	getHistory(type) {
 		let { history } = this.props;
 		const { preferences } = this.props;
-		const { dates, search } = this.state;
+		let { search } = this.state;
+		const { dates } = this.state;
 
 		if (type) {
 			history = history.filter(transaction => transaction.type.startsWith(type));
@@ -98,11 +99,13 @@ class HistoryScreen extends React.Component {
 		);
 
 		if (search !== '') {
+			search = search.toLowerCase();
+
 			history = history.filter(
 				({ name, message, fun }) =>
-					(name && name.includes(search)) ||
-					(message && message.includes(search)) ||
-					(fun && fun.includes(search))
+					(name && name.toLowerCase().includes(search)) ||
+					(message && message.toLowerCase().includes(search)) ||
+					(fun && fun.toLowerCase().includes(search))
 			);
 		}
 
